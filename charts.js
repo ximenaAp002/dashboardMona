aea61d8a39ccbc7d1bf56cfb592c45f46b176260
ximenaAp002 <74875120+ximenaAp002@users.noreply.github.com>
2023-09-24 03:09:15 -0600

    Add files via upload

---
+++ charts.js
@@ -0,0 +1,97 @@
+function generateCharts(data) {
+    // Obtén un arreglo de edades desde los datos filtrados
+    const ages = data.map((item) => parseInt(item.Edad));
+
+    // Obtén un arreglo de perfiles desde los datos filtrados
+    const profiles = data.map((item) => item["Objetivos de Inversión"]);
+
+    // Obtén un arreglo de tolerancias al riesgo desde los datos filtrados
+    const riskTolerances = data.map((item) => item["Tolerancia al Riesgo"]);
+
+    // Llama a las funciones para generar los gráficos
+    generateAgeBarChart(ages);
+    generateProfilePieChart(profiles);
+    generateRiskTolerancePieChart(riskTolerances);
+}
+
+function generateAgeBarChart(ages) {
+    const ctx = document.getElementById("ageChart").getContext("2d");
+    const ageData = {
+        labels: Array.from({ length: 73 }, (_, i) => i + 18), // Edades de 18 a 90
+        datasets: [
+            {
+                label: "Distribución de Edad",
+                data: calculateAgeDistribution(ages),
+                backgroundColor: "rgba(75, 192, 192, 0.2)",
+                borderColor: "rgba(75, 192, 192, 1)",
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    new Chart(ctx, {
+        type: "bar",
+        data: ageData,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                },
+            },
+        },
+    });
+}
+
+function calculateAgeDistribution(ages) {
+    // Calcula la distribución de edades
+    const ageDistribution = Array(73).fill(0); // Inicializa un arreglo de 73 elementos con ceros
+    ages.forEach((age) => {
+        ageDistribution[age - 18]++;
+    });
+    return ageDistribution;
+}
+
+function generateProfilePieChart(profiles) {
+    const ctx = document.getElementById("objectiveChart").getContext("2d");
+    const profileData = getChartDataCount(profiles);
+
+    new Chart(ctx, {
+        type: "pie",
+        data: {
+            labels: profileData.labels,
+            datasets: [
+                {
+                    data: profileData.dataCounts,
+                    backgroundColor: ["#FF5733", "#33FFA1", "#3377FF", "#FF33DD", "#FFFF33"],
+                },
+            ],
+        },
+    });
+}
+
+function generateRiskTolerancePieChart(riskTolerances) {
+    const ctx = document.getElementById("riskToleranceChart").getContext("2d");
+    const riskToleranceData = getChartDataCount(riskTolerances);
+
+    new Chart(ctx, {
+        type: "pie",
+        data: {
+            labels: riskToleranceData.labels,
+            datasets: [
+                {
+                    data: riskToleranceData.dataCounts,
+                    backgroundColor: ["#FF5733", "#33FFA1", "#3377FF"],
+                },
+            ],
+        },
+    });
+}
+
+function getChartDataCount(data) {
+    // Calcula la cantidad de cada tipo en un arreglo de datos
+    const uniqueData = Array.from(new Set(data));
+    const dataCounts = uniqueData.map((value) => {
+        return data.filter((item) => item === value).length;
+    });
+    return { labels: uniqueData, dataCounts };
+}
---
+++ data.js
@@ -0,0 +1,91 @@
+
+function loadDataFromCSV() {
+    // Nombre del archivo CSV
+    const fileName = "data.csv";
+
+    Papa.parse(fileName, {
+        header: true, // Indica que la primera fila contiene encabezados
+        download: true, // Descarga el archivo CSV
+        dynamicTyping: true, // Convierte automáticamente los valores a tipos de datos apropiados
+
+        complete: function (results) {
+            const jsonData = results.data;
+
+            // Llama a la función de filtrado y generación de gráficos
+            applyFiltersAndGenerateCharts(jsonData);
+        },
+    });
+}
+
+// Función para cargar y procesar datos desde el archivo Excel
+function loadDataFromExcel() {
+    // Nombre del archivo Excel
+    const fileName = "data.xlsx";
+
+    // Carga el archivo Excel utilizando SheetJS
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        const data = e.target.result;
+        const workbook = XLSX.read(data, { type: "binary" });
+
+        // Procesa los datos
+        const sheetName = workbook.SheetNames[0]; // Supongamos que los datos están en la primera hoja
+        const worksheet = workbook.Sheets[sheetName];
+        const jsonData = XLSX.utils.sheet_to_json(worksheet);
+
+        // Llama a la función de filtrado y generación de gráficos
+        applyFiltersAndGenerateCharts(jsonData);
+    };
+
+    const req = new XMLHttpRequest();
+    req.open("GET", fileName, true);
+    req.responseType = "blob";
+    req.onload = function () {
+        reader.readAsBinaryString(req.response);
+    };
+    req.send();
+}
+
+// Función para aplicar filtros y generar gráficos
+function applyFiltersAndGenerateCharts(data) {
+    // Obtén los valores seleccionados en los filtros
+    const ageFilter = document.getElementById("ageFilter").value;
+    const objectiveFilter = document.getElementById("objectiveFilter").value;
+    const riskToleranceFilter = document.getElementById("riskToleranceFilter").value;
+
+    // Aplica los filtros
+    let filteredData = data.filter((item) => {
+        return (
+            (!ageFilter || item.Edad === ageFilter) && // Filtrar por edad
+            (!objectiveFilter || item["Objetivos de Inversión"] === objectiveFilter) && // Filtrar por perfil
+            (!riskToleranceFilter || item["Tolerancia al Riesgo"] === riskToleranceFilter) // Filtrar por riesgo
+        );
+    });
+
+    // Actualiza la tabla y los gráficos con los datos filtrados
+    displayTable(filteredData);
+    generateCharts(filteredData);
+}
+
+// Función para mostrar datos en la tabla
+function displayTable(data) {
+    const table = document.getElementById("data-table");
+    table.innerHTML = ""; // Limpia la tabla
+
+    data.forEach((item) => {
+        const row = table.insertRow();
+        for (let key in item) {
+            const cell = row.insertCell();
+            cell.innerHTML = item[key];
+        }
+    });
+}
+
+// Función para generar gráficos
+function generateCharts(data) {
+    // Implementa la generación de gráficos aquí utilizando Chart.js
+    // Por ejemplo, un gráfico de barras para la edad y gráfico de pastel para perfil y riesgo.
+}
+
+// Carga los datos al inicio
+window.onload = loadDataFromCSV;
